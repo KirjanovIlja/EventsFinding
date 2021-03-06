82bfa911d9d586da79ae7d442953ba9bf6f6aad2
Ilja Kirjanov <kirjanovilja@gmail.com>
2021-03-06 23:39:53 +0100

    Link field added to Concert

--- Concert.py
+++ Concert.py
@@ -1,8 +1,8 @@
-class Concert {
-    def __init__(self, band, date, city, place, genre):
+class Concert :
+    def __init__(self, band, date, city, place, genre, link):
         self.band = band
         self.date = date 
         self.city = city
         self.place = place
         self.genre = genre 
-}
+        self.link = link
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
 import re
 from bs4 import BeautifulSoup
+from Concert import Concert
 
 URL = 'https://concertful.com/area/poland/'
 
@@ -8,7 +9,15 @@ page = requests.get(URL)
 soup = BeautifulSoup(page.content, 'html.parser')
 infos = []
 bands = []
+abbrs = []
 
 for i in soup.findAll('div', {'class':'einfo'}):
     infos.append(i)
     bands.append(i.span.a.string)
+    #band = i.abbr.find_all('abbr')[0]['content']
+    #link = i.abbr.find_all('abbr')[1]['content']
+    
+    print(i.find_all('<span class="elocation">'))
+    #print(band, link)
+
+print(infos[0].prettify())
